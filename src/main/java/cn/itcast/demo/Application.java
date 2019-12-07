79419b0d78e11b6b2ab883a7ec57f7e2a629bc7d
Howay <heweihzl@163.com>
2019-12-07 10:32:44 +0800

    热部署成功

--- src/main/java/cn/itcast/demo/Application.java
+++ src/main/java/cn/itcast/demo/Application.java
@@ -1,10 +1,15 @@
 package cn.itcast.demo;
 
+
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
--- src/main/java/cn/itcast/demo/HelloWorldController.java
+++ src/main/java/cn/itcast/demo/HelloWorldController.java
@@ -1,14 +1,26 @@
 package cn.itcast.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
+    @Autowired
+    private Environment env;
+
     @RequestMapping("/info")
     public String info(){
-        return "HelloWorld";
+        return "HelloWorld "+env.getProperty("url");
     }
+
+    @RequestMapping("/info2")
+    public String info2(){
+        return "HelloWorld2 "+env.getProperty("url");
+    }
+
 }
